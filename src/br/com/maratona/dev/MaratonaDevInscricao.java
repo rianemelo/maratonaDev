a6902cdab09604d09d363e192e2127707cea8168
melo <rianemlo@gmail.com>
2020-10-17 15:07:24 -0300

    Java usando servlet. (doGet)

---
+++ src/br/com/maratona/dev/MaratonaDevInscricao.java
@@ -0,0 +1,22 @@
+package br.com.maratona.dev;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/inscricao")
+public class MaratonaDevInscricao extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { //essa é uma checked exception, deve ser tratada forçosamente
+		System.out.println("Teste Maratona!");
+		PrintWriter out = resp.getWriter(); //classe responsável por gerar a resposta
+		out.print("Maratona usando servlet...");
+	}
+	
+}
